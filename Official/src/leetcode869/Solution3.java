1c31ed3c1e17cac229db0c61352a4433e134055d
kallx <2574326165@qq.com>
2021-10-28 16:24:39 +0800

    第二次提交

---
+++ Official/src/leetcode869/Solution3.java
@@ -0,0 +1,32 @@
+package leetcode869;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author Allx
+ * @Date 2021/10/28 16:23
+ */
+public class Solution3 {
+    Set<String> powerOf2Digits = new HashSet<String>();
+
+    public boolean reorderedPowerOf2(int n) {
+        init();
+        return powerOf2Digits.contains(countDigits(n));
+    }
+
+    public void init() {
+        for (int n = 1; n <= 1e9; n <<= 1) {
+            powerOf2Digits.add(countDigits(n));
+        }
+    }
+
+    public String countDigits(int n) {
+        char[] cnt = new char[10];
+        while (n > 0) {
+            ++cnt[n % 10];
+            n /= 10;
+        }
+        return new String(cnt);
+    }
+}
